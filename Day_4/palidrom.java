4535ab77f93c729deeb5e66a7ec77c0c9b30a471
cseboyrohit <rohit887335@gmail.com>
2025-01-06 19:22:52 +0530

    26. Remove Duplicates from Sorted Array

---
+++ Day_4/palidrom.java
@@ -0,0 +1,18 @@
+// public class palidrom {
+// public static void main(String[] args) {
+// Solution solution = new Solution();
+
+// // Test cases
+// int num1 = 121;
+// int num2 = -121;
+// int num3 = 10;
+
+// // Check if each number is a palindrome
+// System.out.println(num1 + " is palindrome? " + solution.isPalindrome(num1));
+// // true
+// System.out.println(num2 + " is palindrome? " + solution.isPalindrome(num2));
+// // false
+// System.out.println(num3 + " is palindrome? " + solution.isPalindrome(num3));
+// // false
+// }
+// }
