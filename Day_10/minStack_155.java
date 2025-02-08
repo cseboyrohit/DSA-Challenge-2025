7f54962a442b8fb3f65d29bb9da727cf4aa6e38b
cseboyrohit <rohit887335@gmail.com>
2025-02-08 18:53:05 +0530

    155 medium problem

--- Day_10/minStack_155.java
+++ Day_10/minStack_155.java
@@ -1,3 +1,53 @@
-public class minStack_155 {
+import java.util.*;
+class MinStack {
+  private Stack<Integer>stack;
+  private Stack<Integer>minStack;
+  public MinStack() {
+      stack=new Stack<>();
+      minStack=new Stack<>();
+      
+  }
   
+  public void push(int val) {
+     stack.push(val);
+       // Push to minStack if it is empty or val is smaller or equal to the top of minStack
+     if(minStack.isEmpty() || val <= minStack.peek()){
+      minStack.push(val);
+     }
+  }
+  
+  public void pop() {
+      int top= stack.pop();
+      if(top == minStack.peek()){
+          minStack.pop();
+      }
+      
+  }
+  
+  public int top() {
+    return stack.peek();
+      
+  }
+  
+  public int getMin() {
+      return  minStack.peek();
+  }
+}
+public class minStack_155 {
+  public static void main(String[]args){
+    MinStack minStack = new MinStack();
+     // Push values
+     minStack.push(-2);
+     minStack.push(0);
+     minStack.push(-3);
+     System.out.println(minStack.getMin()); // returns -3
+
+     minStack.pop();
+
+     // Print the top element
+     System.out.println(minStack.top()); // returns 0
+
+     // Print the minimum value
+     System.out.println(minStack.getMin()); // returns -2
+  }
 }
