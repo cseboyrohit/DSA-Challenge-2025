e22a053acf8789767050191556a3640eb87da5b3
[ʀᴏʜɪᴛ ᴋᴜᴍᴀʀ] <147188266+cseboyrohit@users.noreply.github.com>
2025-03-04 21:57:25 +0530

    Update maxProduct_1464.java

--- Day_13/maxProduct_1464.java
+++ Day_13/maxProduct_1464.java
@@ -14,3 +14,14 @@ class Solution {
             return ans;
     }
 }
+
+// ............................................................................
+import java.util.*;
+class Solution {
+    public int maxProduct(int[] nums) {
+        Arrays.sort(nums);
+        int n=nums.length-1;
+        return (nums[n]-1) * (nums[n-1]-1);
+        
+    }
+}
