1188a1325d45ebbd71dee0e9a9eaf0ce3ad393f1
[ʀᴏʜɪᴛ ᴋᴜᴍᴀʀ] <147188266+cseboyrohit@users.noreply.github.com>
2025-05-06 22:51:30 +0530

    Create matSearch.java

---
+++ Day_26/matSearch.java
@@ -0,0 +1,29 @@
+
+
+class GfG {
+    static boolean matSearch(int[][] mat, int x) {
+        int n = mat.length, m = mat[0].length;
+      
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(mat[i][j] == x)
+                    return true;
+            }
+        }
+      
+        // If x was not found, return false
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[][] mat = {{3, 30, 38},
+                       {20, 52, 54},
+					   {35, 60, 69}};
+        int x = 35;
+        
+        if(matSearch(mat, x)) 
+            System.out.println("true");
+        else 
+            System.out.println("false");
+    }
+}
