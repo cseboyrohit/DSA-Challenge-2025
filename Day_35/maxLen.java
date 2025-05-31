a789a90478a5c6beab8144ac7945072dcfa1bb30
[ʀᴏʜɪᴛ ᴋᴜᴍᴀʀ] <147188266+cseboyrohit@users.noreply.github.com>
2025-05-31 20:06:57 +0530

    Create maxLen.java

---
+++ Day_35/maxLen.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+
+public class Solution {
+    
+    public static int maxLen(int[] arr) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int sum = 0;
+        int res = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            // Convert 0s to -1 to find equal 0s and 1s
+            sum += (arr[i] == 0) ? -1 : 1;
+
+            if (sum == 0) {
+                res = i + 1;
+            }
+
+            if (map.containsKey(sum)) {
+                res = Math.max(res, i - map.get(sum));
+            } else {
+                map.put(sum, i);
+            }
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {0, 1, 0, 1, 1, 0, 0};
+        int result = maxLen(arr);
+        System.out.println("Length of longest subarray: " + result);
+    }
+}
